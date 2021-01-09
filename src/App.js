4aa47d09749098e2ca1fdc5b720c7bae5b059ab4
MakremE88 <ellouzemakrem@gmail.com>
2021-01-09 09:43:25 +0100

    React State

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import {Container, Row, Col} from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+   state = {
+     Person: {
+       fullName: "Makrem Ellouze",
+       bio: "Produit",
+       imgSrc: "./nature.jpg",
+       profession: "Developpeur",
+      },
+       show: true
+    };
+    handleShowPerson = () => {
+      this.setState({
+        show: !this.state.show
+      });
+    
+    };
+    componentDidMount() {
+      setInterval(() => {
+        console.log ("Time since component has mounted")
+      }, 1000)
+    }
 
+    render() {
+      return (
+        <>
+          <Container>
+            <Row className="justify-content-md-center text-center mt-5">
+              {this.state.show && (
+                <>
+                  <Col xs lg="12" className="text-primary"><h1>{this.state.Person.fullName}</h1></Col>
+                  <Col xs lg="12" className="text-secondary"><h2>{this.state.Person.bio}</h2></Col>
+                  <Col xs lg="12" className="text-secondary"><h2>{this.state.Person.profession}</h2></Col><Col xs lg="12" className="mt-3 mb-3"><img src={this.state.Person.imgSrc} width={150} height={150}alt="Nature"></img></Col>
+
+                </>
+              )}
+              <Button variant="outline-primary" className="mt-3" onClick={this.handleShowPerson}>Click here</Button>
+            </Row>
+          </Container>
+        </>
+      );
+    }
+  }
 export default App;
